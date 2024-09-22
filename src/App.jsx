198ad1dbf7c17ce06fb34b1e39b29c525e0bd4f1
mastewal <mastewaldegu0@gmail.com>
2024-09-22 05:42:42 -0300

    update signup page and create navbar page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./nav/Navbar"
 import SignIn from "./signup/signin"
 import SignUp from "./signup/signup"
 function App() {
   return (
     <div >
      
-      <SignIn />
+      <Navbar/>
      
       
     </div>
---
+++ src/nav/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import image from "../assets/nonoo.jpeg"
+function Navbar() {
+  return (
+      <div className='nav'>
+        <img src={image} alt="mdlogo" />
+    
+    </div>
+  )
+}
+
+export default Navbar
